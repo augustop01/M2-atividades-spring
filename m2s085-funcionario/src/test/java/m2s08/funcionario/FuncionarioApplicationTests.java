bfd833c5ba5c28fab16bf18b2e0c135312fc892c
Augusto Pires <91226967+augustop01@users.noreply.github.com>
2023-12-11 01:04:15 -0300

    início do mini-projeto da semana 8

---
+++ m2s085-funcionario/src/test/java/m2s08/funcionario/FuncionarioApplicationTests.java
@@ -0,0 +1,13 @@
+package m2s08.funcionario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuncionarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
